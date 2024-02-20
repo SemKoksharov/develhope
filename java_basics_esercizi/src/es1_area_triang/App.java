607aa42a03a07cec3c9523733bef7b56c072900f
Sem Koksharov <samsungj73965@gmail.com>
2024-02-20 21:22:08 +0100

    Tutto elliminato per cominciare da 0 :D

--- java_basics_esercizi/src/es1_area_triang/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        double a = 10; // base
-        double b = 25.5; // altezza
-        double c = (a * b) / 2; // area
-        System.out.println("Area di triangolo = " + c);
-    }
-}
