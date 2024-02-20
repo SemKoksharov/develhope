d2c335e0c13c681bdad0f8f6a476b136d4c9c2b9
Sem Koksharov <samsungj73965@gmail.com>
2024-02-20 21:39:11 +0100

    Esercizio rifatto bene

---
+++ Es-java-basics/src/Es1_area/Main.java
@@ -0,0 +1,10 @@
+package Es1_area;
+
+public class Main {
+    public static void main(String[] args) {
+        double base = 10;
+        double altezza = 25.5;
+        double area = (base * altezza) / 2;
+        System.out.println("Area di triangolo = " + area);
+    }
+}
