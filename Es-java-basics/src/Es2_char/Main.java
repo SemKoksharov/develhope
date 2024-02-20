3ce114df22e290bc05051877f35f127d1b5455fe
Sem Koksharov <samsungj73965@gmail.com>
2024-02-20 21:59:08 +0100

    Fatto esercizio n2

---
+++ Es-java-basics/src/Es2_char/Main.java
@@ -0,0 +1,9 @@
+package Es2_char;
+
+public class Main {
+    public static void main(String[] args) {
+        char char1 = 'C';
+        char char2 = 'D';
+        System.out.println("Risultato di combinazione = " + char1 + char2);
+    }
+}
