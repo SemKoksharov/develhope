85a52ab665c735aa1f3918b17296ad24c184afb5
Sem Koksharov <samsungj73965@gmail.com>
2024-02-21 23:04:19 +0100

    Fatto esercizio n15

--- Es-java-basics/src/Es15_ifelse_1/Main.java
+++ Es-java-basics/src/Es15_ifelse_1/Main.java
@@ -1,4 +1,22 @@
 package Es15_ifelse_1;
 
 public class Main {
+    public static void main(String args[]){
+        for (int i = 1; i <= 100; i++ ){
+            System.out.println(i + " " +controllo(i));
+        }
+
+    }
+    public static String controllo(int i){
+       String result = "";
+
+       if(i % 3 == 0 && i % 5 == 0){
+            result = "FizzBuzz";
+        } else if (i % 3 == 0) {
+            result = "Fizz";
+        } else if (i % 5 == 0) {
+            result = "buzz";
+        }
+        return result;
+    }
 }
