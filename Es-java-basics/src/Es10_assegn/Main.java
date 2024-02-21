9a6083f515073ad41bd20646c4d59c8756766fd3
Sem Koksharov <samsungj73965@gmail.com>
2024-02-21 18:52:34 +0100

    Fatto esercizio n10

---
+++ Es-java-basics/src/Es10_assegn/Main.java
@@ -0,0 +1,13 @@
+package Es10_assegn;
+
+public class Main {
+    public static void main(String args[]){
+        double a = 22.5;
+        double b = 17.2;
+
+        System.out.println("Risultato di incrementazione di a/b con multiplicazione tra di loro = " + incrAndMiltiplic(a,b));
+    }
+    public static double incrAndMiltiplic(double a, double b){
+        return (a *= 24) * (b *= 87);
+    }
+}
