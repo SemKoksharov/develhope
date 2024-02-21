adde2a2c64dc0a554ccd1ef212b82232d366e8ae
Sem Koksharov <samsungj73965@gmail.com>
2024-02-21 16:41:19 +0100

    AFatto ancora un due esercisi

---
+++ Es-java-basics/src/Es8_aritm_2/Main.java
@@ -0,0 +1,16 @@
+package Es8_aritm_2;
+
+public class Main {
+    public static void main(String arg[]){
+        int a = 60;
+        int b = 20;
+        int c = 10;
+
+        System.out.println("Riultato di divisione tra a/b/c = " + divisionTraTreNumeri(a,b,c));
+
+    }
+    public static double divisionTraTreNumeri(int a, int b, int c){
+        double result = ((double) a / (double) b) / (double) c;
+        return result;
+    }
+}
---
+++ Es-java-basics/src/Es9_aritm_3/Main.java
@@ -0,0 +1,14 @@
+package Es9_aritm_3;
+
+public class Main {
+    public static void main(String agrs[]){
+        double a = 3.7;
+        double b = 19.2;
+        double c = 23.12;
+        System.out.println("Media tra a/b/c = " + media(a, b, c));
+    }
+    public static double media(double a, double b, double c){
+        double d = (a + b + c) / 3;
+        return d;
+    }
+}
