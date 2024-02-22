98f574777cf8041d8be2343b4dea5cd968d16724
Sem Koksharov <samsungj73965@gmail.com>
2024-02-22 23:54:44 +0100

    es 21, 22, 23 sono fatti

--- Es-java-basics/src/Es21_ciclofor_2/Main.java
+++ Es-java-basics/src/Es21_ciclofor_2/Main.java
@@ -6,7 +6,6 @@ public class Main {
         int maxQuantity = 6;
 
        revers(value, maxQuantity);
-
     }
     public static void revers(int value, int maxQuantity){
         for (int i = 0; i < maxQuantity; i++){
