99bb6e1249e5973a25d48364075c7568019652b1
Sem Koksharov <samsungj73965@gmail.com>
2024-02-26 10:51:48 +0100

    esercizio 28

--- Es-java-basics/src/Es_28_arr2d_2/Main.java
+++ Es-java-basics/src/Es_28_arr2d_2/Main.java
@@ -2,10 +2,39 @@ package Es_28_arr2d_2;
 
 public class Main {
     public static void main(String args[]){
-        int newArr2D[][] = {
-                {6, 10, 4},
-                {1, 4, 88}
+        int matrix[][] = {
+                //    columns
+                    {6, 10, 4}, // row
+                    {1, 4, 88}  // row
         };
+        System.out.println("Matrice originale: ");
+        printMatrix(matrix);
 
+        int[][]resultMatrix = swapRowsWithColumns(matrix); //facciamo swap
+
+        System.out.println("\n\nMatrice nuova");
+        printMatrix(resultMatrix);
+
+    }
+    public static int [][] swapRowsWithColumns(int someMatrix[][]){
+        int rows = someMatrix.length;
+        int columns = someMatrix[0].length;
+        int[][] resultMatrix = new int[columns][rows];
+
+        for (int row = 0; row < rows; row++){
+            for (int column = 0; column < columns; column++) {
+                resultMatrix[column][row] = someMatrix[row][column];
+            }
+        }
+        return resultMatrix;
+    }
+    public static void printMatrix(int[][] someMatrix){
+        for (int i = 0; i < someMatrix.length; i++){
+            System.out.println();
+            for (int j = 0; j < someMatrix[i].length; j++ ){
+                System.out.print(someMatrix[i][j] + " ");
+            }
+
+        }
     }
 }
