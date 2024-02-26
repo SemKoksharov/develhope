2b37a5ae408da8be5010a2802959d34ffe9f476b
Sem Koksharov <samsungj73965@gmail.com>
2024-02-26 19:30:49 +0100

    fatto e salvato un piccolo esercizio da codewars

---
+++ Es-java-basics/src/Esrecizio_da_codewars_1/Main.java
@@ -0,0 +1,15 @@
+package Esrecizio_da_codewars_1;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(isLove(4,3));
+    }
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        boolean result = false;
+        if (flower1 % 2 != flower2 % 2) {
+            result = true;
+        }
+        return result;
+    }
+}
