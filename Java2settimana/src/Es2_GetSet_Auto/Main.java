df4ddbb82bf5f77d246e1eb11b9e49da8f8e6d5b
Sem Koksharov <samsungj73965@gmail.com>
2024-02-27 15:45:28 +0100

    кÑerror fondamental deleted

--- Java2settimana/src/Es2_GetSet_Auto/Main.java
+++ Java2settimana/src/Es2_GetSet_Auto/Main.java
@@ -1,7 +1,7 @@
 package Es2_GetSet_Auto;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Car lexus = new Car("Lexus", "GS 300", 3.0f, "DEATH666MET");
         Car mercedes = new Car("Mercedes", "E 220", 2.0f, "GOD15GOOD");
 
