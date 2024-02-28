eb17b2cedaa73fc04a684d557d60e151c1c551a1
Sem Koksharov <samsungj73965@gmail.com>
2024-02-28 13:23:47 +0100

    Improvements in the main class. The setter was applied to the student Vasily.

--- Java2settimana/src/Es1_GetSet_Student/Main.java
+++ Java2settimana/src/Es1_GetSet_Student/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Student vasiliyPetrov = new Student("Vasiliy", "Petrov", 666);
         Student annaKurochkina = new Student("Anna", "Kurochkina", 88);
-        System.out.println(vasiliyPetrov + "\n\n" + annaKurochkina);
+
+        System.out.println("Student card Vasily"+"\nName: "+ vasiliyPetrov.getName()+"\nSurname: "+ vasiliyPetrov.getSurname() + "\nID: "+ vasiliyPetrov.getId());
+        System.out.println("\nStudent card Anna"+"\nName: "+ annaKurochkina.getName()+"\nSurname: "+ annaKurochkina.getSurname() + "\nID: "+ annaKurochkina.getId());
+
+        vasiliyPetrov.setId(6254);
+        System.out.println("\nStudent ID Vasily was changed to: " + vasiliyPetrov.getId());
+        System.out.println("\nStudent card Vasily"+"\nName: "+ vasiliyPetrov.getName()+"\nSurname: "+ vasiliyPetrov.getSurname() + "\nID: "+ vasiliyPetrov.getId());
     }
+
+
+
 }
