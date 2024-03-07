a2e51b5e035a8e517f49f76dbbe501ca4339f9ee
Sem Koksharov <samsungj73965@gmail.com>
2024-03-07 22:42:22 +0100

    alcuni esercizi

--- Es-java-basics/src/Esrecizi_plus/ArrayOfWords2String.java
+++ Es-java-basics/src/Esrecizi_plus/ArrayOfWords2String.java
@@ -1,2 +1,14 @@
-package Esrecizi_plus;public class ArrayOfWords2String {
+package Esrecizi_plus;
+
+import java.util.Arrays;
+
+public class ArrayOfWords2String {
+    public static void main(String[] args) {
+    String [] myWords = {"Suka","Sobaka","Obezyana","Makaka", "Poka-poka loh!", "  x "};
+        System.out.println(smash(myWords));
+
+    }
+    public static String smash (String[] words){
+        return String.join(" ", words);
+    }
 }
--- Es-java-basics/src/Esrecizi_plus/GetSum.java
+++ Es-java-basics/src/Esrecizi_plus/GetSum.java
@@ -1,2 +1,16 @@
-package Esrecizi_plus;public class GetSum {
+package Esrecizi_plus;
+
+public class GetSum {
+    public static void main(String[] args) {
+        System.out.println(sum(1,4)); //1+2+3+4
+        System.out.println(sum(0,-1)); // 0 + (-1)
+    }
+
+    public static int sum(int a, int b){
+        int res = 0;
+        for (int i = a; i<=b; i++){
+            res+=i;
+        }
+        return res;
+    }
 }
