c2d443a67712d0d5030969675eb5babcd92b76a5
SemKoksharov <samsungj73965@gmail.com>
2024-03-12 01:14:52 +0100

    es2 mod JA

--- JavaAdvanced/src/es2_trycatch/Main.java
+++ JavaAdvanced/src/es2_trycatch/Main.java
@@ -1,4 +1,15 @@
 package es2_trycatch;
 
 public class Main {
+    public static void main(String[] args) {
+        isNumber('5');
+    }
+
+    public static void isNumber(char sym) {
+        if (sym <= '9' && sym >= '0') {
+            System.out.println("'" + sym + "'"+ " `e numero.");
+        } else {
+            throw new IllegalArgumentException("Character " + "'" + sym + "'" + " non `e numero");
+        }
+    }
 }
