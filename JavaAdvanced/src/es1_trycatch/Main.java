aca0c4db6c8c225cc4d8241847657cb1a707048c
SemKoksharov <samsungj73965@gmail.com>
2024-03-17 13:11:24 +0100

    correzione es1

--- JavaAdvanced/src/es1_trycatch/Main.java
+++ JavaAdvanced/src/es1_trycatch/Main.java
@@ -3,16 +3,16 @@ package es1_trycatch;
 public class Main {
     public static void main(String[] args) {
         try {
-            controlAndCreateException(17);
+            System.out.println(controlAndCreateException(18));
         } catch (ArithmeticException ae) {
-            System.out.println(ae.getMessage());
+            System.out.println("Errore: " + ae.getMessage());
+            // ho fatto sout per far vedere che metodo rest. TRUE se num `e tra 1 e 17
         }
     }
 
     public static boolean controlAndCreateException(int num) throws ArithmeticException {
         if (num >= 1 && num <= 17) {
-            System.out.println("17 >=Numero >=1\n" + true);
             return true;
-        } else throw new ArithmeticException("Errore! Il numero deve essere tra 1 e 17!");
+        } else throw new ArithmeticException("Il numero deve essere tra 1 e 17!");
     }
 }
