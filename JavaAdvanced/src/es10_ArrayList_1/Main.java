7c69b8090b874a7dfa53d563a333b9bf957542e3
SemKoksharov <samsungj73965@gmail.com>
2024-03-19 11:12:11 +0100

    es10

--- JavaAdvanced/src/es10_ArrayList_1/Main.java
+++ JavaAdvanced/src/es10_ArrayList_1/Main.java
@@ -12,35 +12,38 @@ Aggiungere 4 elementi alla collezione e stampare la collezione aggiornata in con
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Main {
 
-  public static void main(String[]args){
+    public static void main(String[] args) {
 
-      //ho creato 4 studenti
-      Studente studente1 = new Studente("Vasya", 20);
-      Studente studente2 = new Studente("Petya", 27);
-      Studente studente3 = new Studente("Vanya", 24);
-      Studente studente4 = new Studente("Katya", 21);
+        //ho creato 4 studenti
+        Studente studente1 = new Studente("Vasya", 20);
+        Studente studente2 = new Studente("Petya", 27);
+        Studente studente3 = new Studente("Vanya", 24);
+        Studente studente4 = new Studente("Katya", 21);
 
-      //Inizializzato e riempito ArrayList
-      ArrayList<Studente> studenti = new ArrayList<>(Arrays.asList(studente1, studente2, studente3, studente4));
+        //Inizializzato e riempito ArrayList
+        ArrayList<Studente> studenti = new ArrayList<>(Arrays.asList(studente1, studente2, studente3, studente4));
 
-      //stampo lista di studenti
-      System.out.println(studenti);
-      System.out.println();
-      Studente studente5 = new Studente("Alena", 26);
-      Studente studente6 = new Studente("Ksenia", 27);
-      Studente studente7 = new Studente("Vova", 25);
-      Studente studente8 = new Studente("Natasha", 20);
+        //stampo lista di studenti
+        System.out.println(studenti);
+        System.out.println();
+        Studente studente5 = new Studente("Alena", 26);
+        Studente studente6 = new Studente("Ksenia", 27);
+        Studente studente7 = new Studente("Vova", 25);
+        Studente studente8 = new Studente("Natasha", 20);
 
-      //ho aggiunto ancora 4 studenti
-      studenti.add(studente1);
-      studenti.add(studente2);
-      studenti.add(studente3);
-      studenti.add(studente4);
+        //ho aggiunto ancora 4 studenti
+        studenti.add(studente5);
+        studenti.add(studente6);
+        studenti.add(studente7);
+        studenti.add(studente8);
 
-      //stampo lista di studenti aggiornata
-      System.out.println(studenti);
-  }
+        //stampo lista di studenti aggiornata
+        System.out.println(studenti);
+
+
+    }
 
 }
