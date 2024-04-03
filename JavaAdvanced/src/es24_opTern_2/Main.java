2f74502b4cfec65a538711a8a3af3e3b0be48125
SemKoksharov <samsungj73965@gmail.com>
2024-04-03 18:22:19 +0200

    es 24

---
+++ JavaAdvanced/src/es24_opTern_2/Main.java
@@ -0,0 +1,12 @@
+package es24_opTern_2;
+
+//Copia il seguente programma Java e sostutuisci il costrutto if-else con l'operatore ternario
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        return value.length() >= 10 ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10" ;
+    }
+}
