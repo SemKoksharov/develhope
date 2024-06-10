f5d0dfcebacee082a0be8b39c1fa20a20221ff8b
SemKoksharov <samsungj73965@gmail.com>
2024-06-10 15:23:20 +0200

    Interceptors ex 1 fix errors

---
+++ springinterceptors/src/main/java/com/develhope/springiterceptors/entities/Error.java
@@ -0,0 +1,4 @@
+package com.develhope.springiterceptors.entities;
+
+public record Error(String message, String Status, String timestamp) {
+}
--- springinterceptors/src/main/java/com/develhope/springiterceptors/interceptors/LegacyInterceptor.java
+++ springinterceptors/src/main/java/com/develhope/springiterceptors/interceptors/LegacyInterceptor.java
@@ -1,26 +1,56 @@
 package com.develhope.springiterceptors.interceptors;
 
 
+import com.develhope.springiterceptors.entities.Error;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class LegacyInterceptor implements HandlerInterceptor {
+
+    private final ObjectMapper om = new ObjectMapper();
+
+    private String convertObjectToJson(Object object) throws IOException {
+        return om.writeValueAsString(object);
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        if (! request.getRequestURL().toString().contains("/legacy")) {
+        if (!request.getRequestURL().toString().contains("/api/legacy")) {
             return true;
         } else {
-            response.setStatus(HttpStatus.GONE.value());
 
-            throw new Exception("::::::[ LEGACY_INTERCEPTOR_MSG ] :::::: ENDPOINT " +
-                    request.getRequestURL().toString() + " IS BLOCKED ::::::");
+            HttpStatus status = HttpStatus.GONE;
+            String message = "::::::[ LEGACY_INTERCEPTOR_MSG ] :::::: ENDPOINT " +
+                    request.getRequestURL().toString() + " IS BLOCKED ::::::";
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date());
+
+            Error error = new Error(
+                    message,
+                    status.toString(),
+                    timestamp
+            );
+
+            response.setStatus(status.value());
+            System.out.println(message);
+
+            String jsonResponse = convertObjectToJson(error);
+
+
+            response.getWriter().write(jsonResponse);
+
+            return false;
         }
 
     }
